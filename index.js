6f06d90be5ec7a35b92f953984bd47969e28fb04
anthonyapicella <73201390+anthonyapicella@user.noreply.github.com>
2021-04-03 16:50:49 -0700

    started questions array

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
+        {
+            type:'input',
+            message: 'What is the title of your project?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'Please provide a brief description of your project.',
+            name: 'description',
+        },
+    ])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
+// build inquirer questions
 function init() {}
 
 // Function call to initialize app
